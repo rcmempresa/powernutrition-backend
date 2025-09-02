79c7297514bd41d73875124e7b0f9822e318854a
rcmempresa <rcmiranda.empresa@gmail.com>
2025-09-02 19:53:05 +0100

    final

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()
