fe6d4f85ad1d22274d3f36386010f9fc8f7a8e26
rcmempresa <rcmiranda.empresa@gmail.com>
2025-09-02 22:58:28 +0100

    mais alteracoes

--- src/config/db.js
+++ src/config/db.js
@@ -2,18 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
   ssl: {
     rejectUnauthorized: false
   }
 });
-
-pool.connect()
-  .then(() => console.log('Conexão com o PostgreSQL bem-sucedida!'))
-  .catch(err => console.error('Erro ao conectar ao banco de dados', err));
 
 module.exports = pool;
