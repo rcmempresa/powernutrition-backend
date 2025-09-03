8c05b25c7c0e4600ebc97a7dfd46284954e14d30
rcmempresa <rcmiranda.empresa@gmail.com>
2025-09-03 17:07:50 +0100

    mais alteracoes

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,22 +1,12 @@
 const { Resend } = require('resend');
 require('dotenv').config();
 
-// Inicializa o cliente do Resend com a sua chave de API
-// Certifique-se de adicionar RESEND_API_KEY no .env local e no Railway
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-// --- Função Centralizada para Enviar E-mails ---
-/**
- * Envia um e-mail usando o Resend.
- * @param {string} to - Endereço de e-mail do destinatário.
- * @param {string} subject - Assunto do e-mail.
- * @param {string} htmlContent - Conteúdo do e-mail em formato HTML.
- */
 const sendEmail = async (to, subject, htmlContent) => {
   try {
     const fromEmail = process.env.CONFIRMATION_EMAIL_USER;
 
-    // Use a API do Resend para enviar o e-mail
     const { data } = await resend.emails.send({
       from: `"RD Power" <${fromEmail}>`,
       to: [to],
