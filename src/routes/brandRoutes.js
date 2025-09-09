9b400e736a75d49cab0b4c5c54aced1f48b06877
rcmempresa <rcmiranda.empresa@gmail.com>
2025-09-09 20:00:15 +0100

    mais alteracoes

--- src/routes/brandRoutes.js
+++ src/routes/brandRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const brandController = require('../controllers/BrandController');
-const userMiddleware = require('../middlewares/UserMiddleware');
-
-router.use(userMiddleware);
 
 router.get('/listar', brandController.getBrands);
 
